9fca9203d6408582b5c811f31008df8ecdc02a18
Natalhy Corredor <natics07@gmail.com>
2022-10-16 21:59:51 -0500

    creación de app, configuracion pagina de inicio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import sansevieria from './media/sansevieria.jpg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">     
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <section>
+          <h1>Tipos de Plantas</h1>
+          <ul className="BreedCardContainer"></ul>
+          <li className="BreedCard"></li>
+            <div className="ConenedorImagen">
+              <img src={sansevieria} alt="sansevieria" />
+              Foto de <a href="https://unsplash.com/@kellysikkema?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Kelly Sikkema</a> en <a href="https://unsplash.com/es/s/fotos/planta-lengua-de-suegra?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </div>
+            <li className="BreedCard"></li>
+            <div className="ConenedorImagen">
+              <img src={sansevieria} alt="sansevieria" />
+              Foto de <a href="https://unsplash.com/@kellysikkema?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Kelly Sikkema</a> en <a href="https://unsplash.com/es/s/fotos/planta-lengua-de-suegra?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </div>
+
+        </section>
+
+
+      </main>
     </div>
   );
 }
