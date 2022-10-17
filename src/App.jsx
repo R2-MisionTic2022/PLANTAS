04c8014623cb00beb604cd801225b9c4612017d9
Natalhy Corredor <natics07@gmail.com>
2022-10-17 12:49:21 -0500

    componente cardsPlants uso con props

--- src/App.jsx
+++ src/App.jsx
@@ -30,32 +30,32 @@ function App() {
       <main>
         <section>
           <h1>Tipos de Plantas</h1>
+          <h2>Purificadoras de aire</h2>
             <ul className="PlantsCardContainer">
-              <li className="PlantsCard">
-                <div className="ContainerImage">
-                  <img src={sansevieria} alt="sansevieria" />
-                </div>
-                <span className="imgtitle">Sansevieria </span>
-                <span className="imgfont">Foto de <a href="https://unsplash.com/@kellysikkema?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" className="link">Kelly Sikkema</a> en <a href="https://unsplash.com/es/s/fotos/planta-lengua-de-suegra?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" className="link" >Unsplash</a></span>
-              </li>
-
-              <li className="PlantsCard">
-                <div className="ContainerImage">
-                  <img src={monstera} alt="monstera" />
-                </div>
-                <span className="imgtitle">Monstera </span>
-                <span className="imgfont">Foto de <a href="https://unsplash.com/@kellysikkema?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" className="link">Kelly Sikkema</a> en <a href="https://unsplash.com/es/s/fotos/planta-lengua-de-suegra?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" className="link">Unsplash</a></span>
-              </li>
+              <PlantsCard nombrePlanta="Sansevieria" imagenPlanta={sansevieria} />
+              <PlantsCard nombrePlanta="Monstera" imagenPlanta={monstera}/>
             </ul>      
         </section>
       </main>
 
       <footer>
         <div>
-        ©CopyRight 2022 <a href="mailto:natics07@gmail.com" className="link">Nat</a>
+          ©CopyRight 2022 <a href="mailto:natics07@gmail.com" className="link">Nat</a>
         </div>
       </footer>
     </div>
+  );
+}
+
+function PlantsCard(props){
+  return(
+    <li className="PlantsCard">
+      <div className="ContainerImage">
+        <img src={props.imagenPlanta} alt="imagen planta" />
+      </div>
+      <span className="imgtitle">{props.nombrePlanta}</span>
+      <span className="imgfont">Foto de <a href="https://unsplash.com/@kellysikkema?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" className="link">Kelly Sikkema</a> en <a href="https://unsplash.com/es/s/fotos/planta-lengua-de-suegra?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" className="link" >Unsplash</a></span>
+      </li>
   );
 }
 
